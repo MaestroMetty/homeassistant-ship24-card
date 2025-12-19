72818ba988e22a6aef4c0daf3ddde6988f3bd029
Mattia Baldi <67540490+MaestroMetty@users.noreply.github.com>
2025-12-19 22:33:22 +0100

    Updated form

--- load-ha-form.js
+++ load-ha-form.js
@@ -1,6 +1,5 @@
 // Helper to load HA form components if needed
 export async function loadHaForm() {
-  // This is a placeholder - HA form components are usually already loaded
   return Promise.resolve();
 }
 
